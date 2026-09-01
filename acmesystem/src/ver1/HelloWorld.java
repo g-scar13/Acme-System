982c8a3f04820bfe1af7121fa53d1bb6f909f6f4
Gennaro Scarfogliero <gnscarfogliero@gmail.com>
2026-09-01 13:47:18 -0400

    Wrote who is working

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
+		
+		System.out.println("Gennaro is working locally on his branch");
 	}
 
 }
